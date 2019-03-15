8c50b436e5b6e7262b532d0fae2b7b3aec919012
melwyn95 <melwyns.ext@teachforindia.org>
2019-03-15 22:45:55 +0530

    [wip] Timestamp logic intial idea

--- src/application/utils.js
+++ src/application/utils.js
@@ -1,5 +1,22 @@
 const isTrashFolder = (folder) => folder.id === 2;
 
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const WEEK = 7 * DAY;
+const MONTH = 4 * WEEK;
+const YEAR = 52 * WEEK;
+
+const getReadableTimeStamp = timestamp => {
+    const now = new Date().getTime();
+    // if same day -> time
+    // if same week -> day time
+    // if same month -> dd day time
+    // if same year -> dd month time
+    // else -> dd month year time
+}
+
 export {
     isTrashFolder
 }
