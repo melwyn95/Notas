1c6a1a8c0fcb9b0bda7de4eb248f6ccccf735dd7
Melwyn Saldanha <melwyn.s@peopleinteractive.in>
2019-03-16 08:36:36 +0530

    [deploy] First Deploy

--- src/components/TextView.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const TextView = ({ children, style, className }) => <span style={style} className={className}>{children}</span>;
-
-export default TextView;
--- src/contexts/idbContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const idbContext = createContext();
-
-export default idbContext;
