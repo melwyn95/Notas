5e2a2cd442a0bb730dc811beaf403466760bce09
Melwyn Saldanha <melwyn.s@peopleinteractive.in>
2019-03-16 23:27:02 +0530

    Updates

---
+++ precache-manifest.af58f714be3e40a47a0b6c3b8d388401.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb7662d86fc1e9d08e24",
+    "url": "/Notas/static/js/runtime~main.bb7662d8.js"
+  },
+  {
+    "revision": "3dcc649c1acaa764a055",
+    "url": "/Notas/static/js/main.3dcc649c.chunk.js"
+  },
+  {
+    "revision": "e75b51ea77c832a0d4dd",
+    "url": "/Notas/static/js/5.e75b51ea.chunk.js"
+  },
+  {
+    "revision": "22deb7a4c71ffbd1110e",
+    "url": "/Notas/static/js/4.22deb7a4.chunk.js"
+  },
+  {
+    "revision": "b740cebe76fdbd7400f5",
+    "url": "/Notas/static/js/3.b740cebe.chunk.js"
+  },
+  {
+    "revision": "d413fa0b739d17460004",
+    "url": "/Notas/static/js/2.d413fa0b.chunk.js"
+  },
+  {
+    "revision": "3dcc649c1acaa764a055",
+    "url": "/Notas/static/css/main.7d25432b.chunk.css"
+  },
+  {
+    "revision": "22deb7a4c71ffbd1110e",
+    "url": "/Notas/static/css/4.060082a7.chunk.css"
+  },
+  {
+    "revision": "16667373f0cf6cc827fb9f3201a0cc18",
+    "url": "/Notas/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2f7470d21fb2a70eb5e1d066e78a575.js"
+  "/Notas/precache-manifest.af58f714be3e40a47a0b6c3b8d388401.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Notas/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
